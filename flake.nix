95049d544c5ef445a2bda3a84ebe53f65738adb6
ners <ners@gmx.ch>
2022-05-15 18:39:23 +0200

    Use flake-utils to allow multiple systems in flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
-{ 
+{
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      hello-haskell = pkgs.haskellPackages.callCabal2nix "hello-haskell" ./. {};
-    in
-    {
-      defaultPackage.${system} = hello-haskell;
-      devShell.${system} = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          cabal-install
-          ghc
-        ];
-      };
-    };
+    with inputs;
+    with builtins;
+    flake-utils.lib.eachSystem (attrNames nixpkgs.legacyPackages) (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hello-haskell =
+          pkgs.haskellPackages.callCabal2nix "hello-haskell" ./. { };
+      in {
+        defaultPackage = hello-haskell;
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ cabal-install ghc ];
+        };
+      });
 }
