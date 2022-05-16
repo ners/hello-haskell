9ecc1feeee1db30f68ef29738e2e52fbfc2f3ec9
ners <ners@gmx.ch>
2022-05-16 09:02:17 +0200

    Add flake compat

---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat = import fetchTarball
+    {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+    { src = ./.; };
+in
+flake-compat.defaultNix
--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
   outputs = inputs:
     with inputs;
@@ -11,7 +15,8 @@
         pkgs = nixpkgs.legacyPackages.${system};
         hello-haskell =
           pkgs.haskellPackages.callCabal2nix "hello-haskell" ./. { };
-      in {
+      in
+      {
         defaultPackage = hello-haskell;
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ cabal-install ghc ];
